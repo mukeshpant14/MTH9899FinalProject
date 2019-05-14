d33ab3512a88cf9e62df7fd3294eb5b64b819a55
Jose Ferreira <joseferreira.tky@gmail.com>
2019-05-13 20:17:33 -0400

    Update final_project.py
    Added:
    - data cleanup, discard securities with missing data
    - normalization per security
    - stationarity of vol via fractional differentiation
    - best method so far: random forest (~25bps out of sample)

--- final_project.py
+++ final_project.py
@@ -27,6 +27,51 @@ from sklearn.ensemble import AdaBoostClassifier, GradientBoostingRegressor, Rand
 #from xgboost import XGBClassifier
 from sklearn.cluster import KMeans
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.preprocessing import StandardScaler
+
+def ts_normalize(df, columns):
+    result = df.copy()
+    sc = StandardScaler()
+    #scale variables along time in the same security
+    for col in columns:
+        result[col] = sc.fit_transform(np.array(result[col]).reshape(-1,1))  
+    return result
+
+def ts_fillna(df, column, default_value=0):
+    result = df.copy()
+    #backfill missing data, try the previous value, if not existing use default 
+    na_rows = result[column].isna().sum()
+    if na_rows > 0:
+        result.vol.fillna(method='ffill', inplace=True)    
+        result.vol.fillna(default_value, inplace=True)          
+    return result
+
+def get_weight_ffd(d, thres, lim):
+  w, k = [1.], 1
+  ctr = 0
+  while True:
+    w_ = -w[-1] / k * (d - k + 1)
+    if abs(w_) < thres:
+      break
+    w.append(w_)
+    k += 1
+    ctr += 1
+    if ctr == lim - 1:
+      break
+  w = np.array(w[::-1]).reshape(-1, 1)
+  return w
+
+def frac_diff_ffd(x, d, thres=1e-3):
+  w = get_weight_ffd(d, thres, len(x))
+  width = len(w) - 1
+  output = []
+  output.extend([0] * width)
+  for i in range(width, len(x)):
+    output.append(np.dot(w.T, x[i - width:i + 1])[0])
+  return np.array(output)
+
+def add_vol_ffd(series, fd):
+  series['vol_ffd'] = frac_diff_ffd(series.vol, fd)    
 
 def pre_process(df, normalize=False):
     secs = df['sec_id'].unique()
@@ -50,12 +95,53 @@ def pre_process(df, normalize=False):
 #        df[''] = preprocessing.normalize(df[''])
 
     return df
-    
-def clean_data(org_data):      
-    median = org_data['vol'].median()
-    org_data['vol'].fillna(median, inplace=True)
 
-    return org_data
+
+def clean_data(org_data, empty_thres=0.25):      
+  org_data.drop('Time',axis=1,inplace=True)
+  # looks like vol=0 is a data capture error, will fill with previous 
+  # value/dataset median
+  org_data.vol.replace(0,np.nan,inplace=True)
+  
+  # extract time-series per security
+  sec_ids = org_data['sec_id'].unique()
+  series = [org_data[org_data['sec_id']== i] for i in sec_ids]
+  series = [ x.set_index('Date') for x in series] #sets the index to the Date
+  
+  
+  # get rid of the securities with less than empty_thres % valid return or vol data (zeros)
+  empty_secs = set()
+  for i in range(len(series)):
+    s = series[i]
+    total = s.shape[0]
+    zeros = len(s[s.fut_ret==0.0])
+    if zeros>total*empty_thres:
+        empty_secs.add(s.sec_id[0])
+    zeros = len(s[s.vol==0.0])
+    if zeros>total*empty_thres:
+        empty_secs.add(s.sec_id[0])
+  
+  print("Discarding " + str(len(empty_secs)) + " securities with incomplete data (threshold=" + str(empty_thres))
+  clean_series = []
+  for s in series:
+    if s.sec_id[0] not in empty_secs:
+        clean_series.append(s)
+  
+  #normalize data and fill empty records
+  median = org_data['vol'].median()
+  input_vars = ['vol','X1','X2','X3','X4','X5','X6','X7']
+  clean_series = [ ts_fillna(x, 'vol', median) for x in clean_series] 
+  clean_series = [ ts_normalize(x, input_vars) for x in clean_series] 
+  
+  #make vol a stationary variable using fractional differencing
+  #the differencing factor is decided as the value that makes more than 95% 
+  #of the samples in the dataset stationary
+  [add_vol_ffd(x, 0.7) for x in clean_series]
+  
+  org_data = pd.concat(clean_series)
+    
+  #TODO normalize again along the entire set?
+  return org_data
     
 def run_models_cross_validation(models):
     
@@ -68,7 +154,8 @@ def get_data():
     return df
 
 def get(df):
-    X = df[['vol', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7']].values
+    #X = df[['vol', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7']].values
+    X = df[['vol_ffd', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7']].values
     y = df[['fut_ret']].values.flatten()
     return (X, y)
 
@@ -158,12 +245,12 @@ def run():
     df = get_data()
     df = pre_process(df)
     df = clean_data(df)
-    
+    df.reset_index(inplace=True)
     # plot for a sec_id = 0
     sec_0 = df.loc[df['sec_id'] == 0]
     sec_0 = sec_0[['Date','fut_ret']]
     sec_0.plot(kind='line',x='Date', y='fut_ret',ax=plt.gca())
-    plt.show()
+    #plt.show()
     
     (X, y) = get(df)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/6, shuffle=False)
@@ -173,10 +260,7 @@ def run():
                    'KNeighborsRegressor', 
                    'DecisionTreeRegressor', 
                    'RandomForestRegressor',
-                   'GradientBoostingRegressor',
-                   'ClusterLinearRegressor',
-                   'ClusterGradientBoostingRegressor']
-    model_names = ['GradientBoostingRegressor','ClusterGradientBoostingRegressor']
+                   'GradientBoostingRegressor']
     
     # fit
     result = {}
